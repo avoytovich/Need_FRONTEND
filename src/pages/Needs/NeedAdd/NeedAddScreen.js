ffc7d2a7fb94681654175c16f988796b658fcd94
avoytovich <levanwork@gmail.com>
2023-02-21 23:36:25 +0100

    add: validation need offer

--- src/pages/Needs/NeedAdd/NeedAddScreen.js
+++ src/pages/Needs/NeedAdd/NeedAddScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useReducer, useState } from 'react';
 
 import NeedAddView from './NeedAddView';
 
@@ -6,6 +6,25 @@ const NeedAddScreen = ({ handleClose, setPage, totalItems }) => {
   const [title, setTitle] = useState('');
   const [abilityToPay, setAbilityToPay] = useState('');
   const [description, setDescription] = useState('');
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'TITLE':
+        return { ...state, title: !!action.payload };
+      case 'ABILITY_TO_PAY':
+        return { ...state, abilityToPay: !!action.payload };
+      case 'DESCRIPTION':
+        return { ...state, description: !!action.payload };
+      default:
+        throw new Error();
+    }
+  };
+
+  const [errors, dispatchError] = useReducer(reducer, {
+    title: false,
+    abilityToPay: false,
+    description: false,
+  });
 
   return (
     <NeedAddView
@@ -18,6 +37,8 @@ const NeedAddScreen = ({ handleClose, setPage, totalItems }) => {
       setAbilityToPay={setAbilityToPay}
       description={description}
       setDescription={setDescription}
+      errors={errors}
+      dispatchError={dispatchError}
     />
   );
 };
